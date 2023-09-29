afc2ded8b260bd4e2a0089f395ccbd778f45dd7d
stupidcupid135 <121999608+stupidcupid135@users.noreply.github.com>
2023-09-29 12:40:18 -0700

    edit files

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
+var abilityScores = {
+    "str": 0,
+    "dex": 0,
+    "con": 0,
+    "int": 0,
+    "wis": 0,
+    "cha": 0,
+};
+var charName = '';
+var charClass = '';
+
 function randomizeStats() {
-    var abilityScores = {
+    var tempScores = {
         "str": randomStat(),
         "dex": randomStat(),
         "con": randomStat(),
@@ -8,6 +19,8 @@ function randomizeStats() {
         "wis": randomStat(),
         "cha": randomStat(),
     };
+    abilityScores = tempScores;
+
     document.getElementById("statBox").innerHTML = "Str: " + abilityScores["str"]
     + "<br> Dex: " + abilityScores["dex"]
     + "<br> Con: " + abilityScores["con"]
@@ -17,3 +30,13 @@ function randomizeStats() {
 }
 
 let randomStat = (stat = 0) => Math.round(stat + (Math.random() * 6 + 1) + (Math.random() * 6 + 1) + (Math.random() * 6 + 1));
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const data = Object.fromEntries(new FormData(e.target).entries());
+    console.log(data)
+    charName = data['name'];
+    charClass = data['class'];
+    abilityScores[data['raisedScore']] = 14;
+    console.log(charName + charClass + abilityScores['dex'])
+  });
